7591a1ab35eb1d8e01a30ed4bd09856e932aae32
MuhDanyMaruf <114630218+MuhDanyMaruf@users.noreply.github.com>
2022-12-15 10:55:53 +0700

    PHP part2

---
+++ coba.php
@@ -0,0 +1,55 @@
+<html>
+
+<head>
+
+</head>
+
+
+<body>
+    <h1>hello world </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda asperiores optio tempore. Inventore non
+        consectetur repellat eligendi et, placeat error hic laboriosam ducimus porro totam nihil laudantium sint
+        nesciunt vel officia possimus voluptate reiciendis dignissimos id suscipit voluptas esse! Nam voluptas iusto
+        commodi exercitationem corrupti unde minima cumque doloribus aut id magnam facere excepturi delectus voluptatum
+        doloremque, dolores sit labore ab dolore facilis aliquam asperiores voluptatibus ipsum molestiae. Vitae at
+        provident laborum doloribus quaerat numquam aut, mollitia eaque, eveniet beatae corporis corrupti accusantium
+        sequi nemo molestias aperiam quis, obcaecati quo fugiat distinctio dolore adipisci. Atque in natus voluptates
+        corrupti quae?</p>
+
+    <?php
+    $txt = "PHP";
+    echo "I love $txt!";
+    ?>
+
+    <?php
+    $color = "red";
+    echo "My car is " . $color . "<br>";
+    echo "My house is " . $COLOR . "<br>";
+    echo "My boat is " . $coLOR . "<br>";
+    ?>
+
+    <?php
+    echo "<h2>PHP is Fun!</h2>";
+    echo "Hello world!<br>";
+    echo "I'm about to learn PHP!<br>";
+    echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+    ?>
+
+    <?php
+    print "<h2>PHP is Fun!</h2>";
+    print "Hello world!<br>";
+    print "I'm about to learn PHP!";
+    ?>
+
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            
+        </body>
+    </html>
+    
+</body>
+
+</html>
---
+++ if.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>if</title>
+</head>
+
+<body>
+    <h2>Penggunaan Kontrol If</h2>
+    <?php
+    $umur = 18;
+    echo ("Umur anda $tahun tahun <br>");
+    if ($umur >= 17) {
+        echo ("Anda boleh masuk");
+    }
+    ?>
+</body>
+
+</html>
